25e5c16f045f127e71b57552c3c7e891678fcaf4
tokyocn <tokyocn@hotmail.com>
2024-03-28 19:16:45 +0900

    mod

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth/next";
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/libs/auth";
 import prisma from "@/app/libs/prismadb";
 
 export async function getSession() {
